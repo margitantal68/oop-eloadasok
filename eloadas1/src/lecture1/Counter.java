c8f104e50a5a95df35737bbdad8e5763183182de
Margit Antal <manyi@ms.sapientia.ro>
2021-02-25 16:26:55 +0200

    tovabbi modulok hozzaadva

--- eloadas1/src/lecture1/Counter.java
+++ eloadas1/src/lecture1/Counter.java
@@ -4,6 +4,9 @@ public class Counter {
     private int value;
 
     //arnyekolas, shadowing
+    // this.value - attributum
+    // value - parameter (lokalis)
+    // a lokalis value elarnyekolja a value attributumot, ezert --> this.value
     public Counter(int value) {
         this.value = value;
     }
